26d72074208420e056013aedeb7757e5af0170dc
Mextli <luna.vazquezv@gmail.com>
2019-03-10 00:42:23 -0600

    se evalua si existen peliculas en la base de datos en boostrap.js

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -13,33 +13,37 @@ const fetch = require('node-fetch');
 const hash = require('object-hash');
 
 module.exports.bootstrap = async function() {
-  // Obtener listado de movies
-  return;
-  const movies = await fetch('https://swapi.co/api/films/').then(response => response.json());
-  for (let movie of movies.results) {
-    // Guardar movie en db con el metodo encuentra o crea
-    await Movie.findOrCreate(
-      {
-        episode: movie.episode_id,
-      },
-      {
-        movieName: movie.title,
-        episode: movie.episode_id,
-      },
-    );
-    for (let starshipUrl of movie.starships) {
-      const starship = await fetch(starshipUrl).then(response => response.json());
-      const starshipId = hash(starship.name + movie.episode_id);
-      await Starship.findOrCreate(
-        { starshipId },
+  //  Se evalua que existan peliculas en la DB
+  const countMovies = await Movie.count();
+  if (!countMovies) {
+    // Obtener listado de movies
+
+    const movies = await fetch('https://swapi.co/api/films/').then(response => response.json());
+    for (let movie of movies.results) {
+      // Guardar movie en db con el metodo encuentra o crea
+      await Movie.findOrCreate(
         {
-          starshipId,
-          episodeId: movie.episode_id,
-          starshipName: starship.name,
-          model: starship.model,
-          passengers: starship.passengers,
+          episode: movie.episode_id,
+        },
+        {
+          movieName: movie.title,
+          episode: movie.episode_id,
         },
       );
+      for (let starshipUrl of movie.starships) {
+        const starship = await fetch(starshipUrl).then(response => response.json());
+        const starshipId = hash(starship.name + movie.episode_id);
+        await Starship.findOrCreate(
+          { starshipId },
+          {
+            starshipId,
+            episodeId: movie.episode_id,
+            starshipName: starship.name,
+            model: starship.model,
+            passengers: starship.passengers,
+          },
+        );
+      }
     }
   }
 };
